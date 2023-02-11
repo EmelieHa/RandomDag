177ae35ccbd0b9c0307f896db968cdf3a0b3044f
EmelieHa <108530359+EmelieHa@users.noreply.github.com>
2023-02-11 11:25:55 +0100

    Update script.js

--- script.js
+++ script.js
@@ -10,7 +10,7 @@ function calculateDay() {
 }
 
 function showDay() {
-    let num = Math.floor(Math.random() * 6);
+    let num = Math.floor(Math.random() * 6) +1;
 switch (num) {
     case 1:
         return 'måndag';
